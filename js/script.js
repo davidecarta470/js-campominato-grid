2bd749d6b4c1c2533477853e9efdde527f6b37bc
davidecarta470 <cartadavi@gmail.com>
2021-11-03 12:33:16 +0100

    correction html structure

--- js/script.js
+++ js/script.js
@@ -1,33 +1,98 @@
-
-
-let numberOfSquare = 0;
+const main = document.getElementById('main');
+// const easyContainer = document.querySelector('.easy-container'); 
+// const averageContainer = document.querySelector('.average-container');
+// const crazyContainer = document.querySelector('.crazy-container');
 const selectValue = document.getElementById('my-select');
-console.log(selectValue.value)
+const Container = document.createElement('div');
+Container.className ='container';
+// let listNumberEasy =  [];
+// let listNumberAverage =  [];
+// let listNumberCrazy =  [];
 
+
+// Creo i tre Container con diverso numero di celle e diverse caratteristiche
+// for(let i=0; i<100; i++){
+//   let easySquare = document.createElement('div');
+//   easySquare.className= 'square-easy';
+//   easySquare.innerHTML = generateUniqueRandomInt(listNumberEasy,1,100);
+//   easyContainer.append(easySquare);
+// }
+
+// for (let i=0 ; i<81 ; i++){
+//   let averageSquare = document.createElement('div');
+//   averageSquare.className ='square-average';
+//   averageSquare.innerHTML = generateUniqueRandomInt(listNumberAverage,1,81);
+//   averageContainer.append(averageSquare);
+// }
+
+// for (let i=0 ; i<49 ; i++){
+//   let crazySquare = document.createElement('div');
+//   crazySquare.className ='square-crazy';
+//   crazySquare.innerHTML = generateUniqueRandomInt(listNumberCrazy,1,49);
+//   crazyContainer.append(crazySquare);
+// }
+
+// Attivo Disattivo display
 const play= document.getElementById('play');
-
 play.addEventListener('click',function(){
-  switch (selectValue.value){
-    case 'easy':
-      numberOfSquare=100;
-      break;
-    case 'average':
-      numberOfSquare=64;
-      break;
-    case 'crazy':
-      numberOfSquare=36;
-      break;
-    default:
-     console.log(`Sorry, we are out of`);
+  let n =0;
+  let listNumber = [];
+  if (selectValue.value ==='easy'){
+    n = 100;
   }
+  if (selectValue.value ==='average'){
+    n = 81;
+  }
+  if (selectValue.value ==='crazy'){
+    n =49;
+  }
+  //  let squareSize=  selectValue.value
 
-  console.log(numberOfSquare)
-});
+
+  Container.innerHTML = '';
+  main.append(Container)
 
 
-const container = document.querySelector('.container');
-for(let i=0 ; i<numberOfSquare; i++){
-const square = document.createElement('div')
-square.className='square';
-container.append(square);
+  for (let i=0 ; i<n ; i++){
+    let Square = document.createElement('div');
+    Square.className ='square';
+    Square.classList.add(selectValue.value)
+    Square.innerHTML = generateUniqueRandomInt(listNumber, 1 , n);
+    Container.append(Square);
+  }
+  
+  //   crazyContainer.classList.remove('active')
+  //   averageContainer.classList.remove('active')
+  //   easyContainer.classList.remove('active')
+
+  // if(selectValue.value ==='easy'){
+    //   easyContainer.classList.add('active')
+  // }
+  
+  // if(selectValue.value ==='average'){
+  //   averageContainer.classList.add('active')
+  // }
+  
+  // if(selectValue.value ==='crazy'){
+    //   crazyContainer.classList.add('active')
+    // }
+    
+
+});  
+  
+ function generateUniqueRandomInt(list, min, max){
+  let number = null, valid = false;
+  while(!valid){
+      number = generateRandomInt(min,max);
+      if(!list.includes(number)) {
+          valid = true;
+          list.push(number);
+      }
+  }
+  return number;
 }
+function generateRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
